b68de0261223261bc204def20ad28d6cd60a66c1
Aaron Lim <104383693+AALim1091@users.noreply.github.com>
2022-05-10 15:02:16 -0700

    Add files via upload

---
+++ C#/Practice/String-Manipulation/prob6_Check if a string is a palindrome or not/prob6_Check if a string is a palindrome or not/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob6_Check_if_a_string_is_a_palindrome_or_not
+{
+    internal class Program
+    {
+        /*
+         * Problem 6: Check if a string is a palindrome or not.
+            * Solution
+
+            Here we have to check that a string is palindrome or not. 
+            Palindrome means when you read it from the beginning or from the last, 
+            characters should be the same.
+            Here, we will check each character from the beginning and compare it from last. 
+        */
+        static void Main(string[] args)
+        {
+            string str = "";
+            Console.WriteLine("Please enter a string");
+            str = Console.ReadLine(); //My name is mukesh
+            Console.WriteLine("");
+
+            checkString(str);
+
+            bool palindrome;
+            string reverseString = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseString += str[i];
+            }
+
+            if(str == reverseString)
+            {
+                palindrome = true;
+            }
+            else
+            {
+                palindrome = false;
+            }
+
+            Console.WriteLine("Is the string \"{0}\" a PALINDROME?: {1} ", str, palindrome );
+            Console.WriteLine("");
+
+
+
+
+
+
+        }
+        static void checkString(string str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+            {
+                Console.WriteLine("You did not enter a valid string, please restart and try again");
+                Console.WriteLine("");
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
